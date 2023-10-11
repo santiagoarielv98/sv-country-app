0a5934234ce6286460ba337ea32c7f5ad94e15cf
Santiago Ariel <santiagoarielv98@gmail.com>
2023-10-11 11:35:07 -0300

    nueva pagina Detail y configuracion en routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,24 @@
 import { createHashRouter } from "react-router-dom";
-import App from "../App";
+
+import Layout from "../layout/Layout";
 
 const routes = createHashRouter([
   {
-    path: "/",
-    element: <App />,
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        lazy: async () => ({
+          Component: (await import("../pages/Home")).default,
+        }),
+      },
+      {
+        path: "/country/:countryName",
+        lazy: async () => ({
+          Component: (await import("../pages/Detail")).default,
+        }),
+      },
+    ],
   },
 ]);
 
