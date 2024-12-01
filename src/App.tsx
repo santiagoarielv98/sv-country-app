ac355999f36f0eba3668fda3d87525327bc27104
Santiago Ariel <santiagoarielv98@gmail.com>
2024-12-01 20:21:27 -0300

    enhance routing configuration; enable future features and improve lazy loading for country components

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,14 @@ import { RouterProvider } from "react-router-dom";
 import routes from "./routes";
 
 const App = () => {
-  return <RouterProvider router={routes} />;
+  return (
+    <RouterProvider
+      router={routes}
+      future={{
+        v7_startTransition: true,
+      }}
+    />
+  );
 };
 
 export default App;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,24 +2,35 @@ import { createHashRouter } from "react-router-dom";
 
 import MainLayout from "@/layout/MainLayout";
 
-const routes = createHashRouter([
+const routes = createHashRouter(
+  [
+    {
+      element: <MainLayout />,
+      children: [
+        {
+          path: "/",
+          lazy: async () => ({
+            Component: (await import("@/features/countries/CountryList")).default,
+          }),
+        },
+        {
+          path: "/country/:countryName",
+          lazy: async () => ({
+            Component: (await import("@/features/countries/CountryDetail")).default,
+          }),
+        },
+      ],
+    },
+  ],
   {
-    element: <MainLayout />,
-    children: [
-      {
-        path: "/",
-        lazy: async () => ({
-          Component: (await import("@/features/countries/CountryList")).default,
-        }),
-      },
-      {
-        path: "/country/:countryName",
-        lazy: async () => ({
-          Component: (await import("@/features/countries/CountryDetail")).default,
-        }),
-      },
-    ],
+    future: {
+      v7_fetcherPersist: true,
+      v7_normalizeFormMethod: true,
+      v7_partialHydration: true,
+      v7_relativeSplatPath: true,
+      v7_skipActionErrorRevalidation: true,
+    },
   },
-]);
+);
 
 export default routes;
